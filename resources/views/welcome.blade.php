f8d1fcb40067603e69937e1860a9fd524697a396
DanielMarella <marella.daniel96@gmail.com>
2023-07-20 16:27:53 +0200

    create cards  for visualize my movie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,22 @@
 
 @section('main-content')
 
-
-@dd($movies)
-
+<div class="container mt-5 pt-5">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="card m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Title: {{$movie->title}}</h5>
+                <h5 class="card-title">Original Title: {{$movie->original_title}}</h5>
+                <p class="card-text">
+                    Nationality: <strong>{{$movie->nationality}}</strong> <br>
+                    Date: <strong>{{$movie->date}}</strong> <br>
+                    Vote: <strong>{{$movie->vote}}</strong>
+                </p>
+            </div>
+        </div>
+    @endforeach
+    </div>
+</div>
+    
 @endsection
